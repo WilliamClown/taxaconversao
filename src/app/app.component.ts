0a0c4e72504516ef11c445bb1e37d30d81bfffbb
William Inacio <williaminacio@hotmail.com>
2024-12-06 13:24:37 -0300

    novos ajustes de layout do menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -18,4 +19,26 @@ import { MatListModule } from '@angular/material/list';
   templateUrl: './app.component.html', // Template principal da aplicação
   styleUrls: ['./app.component.css'],  // Estilos globais
 })
-export class AppComponent {}
+export class AppComponent {
+  isSmallScreen: boolean = false;
+  isMenuOpen = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth <= 768;
+    }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isSmallScreen = window.innerWidth <= 768;
+    }
+  }
+  
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
